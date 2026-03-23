1788b9c48115260017b2d775134352af98e76906
Michael Agubi <mike.1985@live.se>
2026-03-23 22:39:34 +0100

    add treatment controller and services

---
+++ HairdresserAppointment.API/Controllers/AuthController.cs
@@ -0,0 +1,32 @@
+﻿using HairdresserAppointment.API.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HairdresserAppointment.API.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+
+        private readonly UserManager<CustomUser> _userManager;
+        private readonly SignInManager<CustomUser> _signInManager;
+
+        public AuthController(UserManager<CustomUser> userManager, SignInManager<CustomUser> signInManager)
+        {
+            
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
--- HairdresserAppointment.API/Services/TreatmentService.cs
+++ HairdresserAppointment.API/Services/TreatmentService.cs
@@ -1,4 +1,6 @@
 ﻿using HairdresserAppointment.API.Data;
+using HairdresserAppointment.API.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace HairdresserAppointment.API.Services
 {
@@ -7,8 +9,61 @@ namespace HairdresserAppointment.API.Services
         private readonly MyDbContext _context;
 
         public TreatmentService(MyDbContext context)
+
         {
             _context = context;
         }
+
+
+        public async Task<List<Treatment>> GetAllTreatmentsAsync()
+        {
+            return await _context.Treatments.ToListAsync();
+        }
+
+
+        public async Task<Treatment> CreateTreatmentAsync(Treatment treatment)
+        {
+            _context.Treatments.Add(treatment);
+            await _context.SaveChangesAsync();
+
+            return treatment;
+        }
+
+        public async Task<bool> UpdateTreatmentAsync(int id, Treatment treatment)
+        {
+            var updateTreatment = await _context.Treatments.FindAsync(treatment.Id);
+            if (updateTreatment == null)
+                return false;
+
+            updateTreatment.Name = treatment.Name;
+            updateTreatment.Price = treatment.Price;
+            updateTreatment.DurationInMinutes = treatment.DurationInMinutes;
+
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<bool> DeleteTreatmentAsync(int id)
+        {
+            var treatment = await _context.Treatments.FindAsync(id);
+            if (treatment == null)
+                return false;
+
+            _context.Treatments.Remove(treatment);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
