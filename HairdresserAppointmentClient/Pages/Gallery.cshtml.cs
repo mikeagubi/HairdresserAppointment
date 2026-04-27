6b7a78822d0b9ef5e2d3058908f06ba4b05dd1bf
Michael Agubi <mike.1985@live.se>
2026-04-27 11:03:45 +0200

    add small fixes on karusell btn and background

---
+++ HairdresserAppointmentClient/Pages/Gallery.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HairdresserAppointmentClient.Pages
+{
+    public class GalleryModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
