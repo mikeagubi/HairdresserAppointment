d68f35f59f505558698852486608aebbe2fe7f8e
Michael Agubi <mike.1985@live.se>
2026-05-17 19:28:19 +0200

    add and show all selected treat in basket

--- HairdresserAppointmentClient/Pages/Booking/SelectServices.cshtml.cs
+++ HairdresserAppointmentClient/Pages/Booking/SelectServices.cshtml.cs
@@ -18,27 +18,35 @@ namespace HairdresserAppointmentClient.Pages.Booking
         public List<TreatmentDto> Treatments { get; set; } = new();
         public List<HairdresserDto> Hairdressers { get; set; } = new();
 
-        [BindProperty]
-        public List<TreatmentDto> SelectedTreatments { get; set; } = new();
+
 
         [BindProperty]
         public int HairdresserId { get; set; }
+
+        [BindProperty]
+        public List<int> TreatmentIds { get; set; } = new();
 
 
 
         public async Task OnGet()
         {
+            await LoadPageAsync();
+
+        }
+
+
+        public async Task<IActionResult> OnPostAddTreatmentsAsync(int treatmentId)
+        {
+            TreatmentIds.Add(treatmentId);
+            await LoadPageAsync();
+            return Page();
+        }
+
+        private async Task LoadPageAsync()
+        {
             Treatments = await _treatmentApiService.GetAllTreatmentsAsync();
             Hairdressers = await _hairdresserApiService.GetHairdressersAsync();
-
         }
-
-        public async Task OnPostAsync()
-        {
-
-        }
-
-
 
 
 
